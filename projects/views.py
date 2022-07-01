e7c9971671a6a16d8b5d818553e5974cf20a6dcf
sumedhw <sumedhw71@gmail.com>
2022-07-02 01:26:15 +0530

    projects urls added

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
+from curses.ascii import HT
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def projects(request):
+    return HttpResponse("Projects list")
+
+def project(request,pk):
+    return HttpResponse(f"This is project {pk}")
